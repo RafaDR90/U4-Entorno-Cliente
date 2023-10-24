2b03722900bdd98ed020f47277dcd331cd893b35
Rafa <rafa18220delgado@gmail.com>
2023-10-24 20:05:41 +0200

    WS4 style

--- WS2/index.js
+++
@@ -1,8 +0,0 @@
-const normal=document.getElementById("normal");
-const basico=document.getElementById("basico");
-
-const body=document.querySelector("body");
-
-normal.addEventListener("click",()=>{
-    body.style.paddingLeft="100px"
-})
